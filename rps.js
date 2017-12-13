764e5536604b4a180a1e67d93bf2da989b225f13
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2017-12-13 11:27:49 +0000

    Rock, paper, scissor game done, if/else function could be improved

--- rps.js
+++ rps.js
@@ -7,9 +7,10 @@ var loop = "y";
 function Playerpick(){
 	return prompt("Choose r(ock), p(aper) or (s)cissors!");	
 }
+
 function AIpick(){
 	var generator = Math.floor(Math.random()*3);
-	var aiRandom;
+	var aiRandom ="";
 	if (generator === 0){
 	    aiRandom = "r";
 	}
@@ -22,70 +23,66 @@ function AIpick(){
 	return aiRandom;
 }
 
-p1 = Playerpick();
-randomChoice = AIpick();
 
-function calculation(){
-	if (p1 === "r"){
-		if 	(randomChoice === "r")
+function calculation(p1, randomChoice){
+	if (p1 === "r" && randomChoice == "r"){
 			return 0;
-		else if (randomChoice=== "p")
+	}else if (p1 ==="r" && randomChoice === "p"){
 			return -1;
-		else //(randomChoice === "s")
+	}else if (p1 ==="r" && randomChoice === "s"){
 			return 1;
-	}	
-	else if (p1 === "p"){
-		if 	(randomChoice === "r")
+	}else if (p1 ==="p" && randomChoice === "r"){
 			return 1;
-		else if (randomChoice === "p")
+	}else if (p1 ==="p" && randomChoice === "p"){
+			return 0;	
+	}else if (p1 ==="p" && randomChoice === "s"){
+			return -1;
+	}else if (p1 ==="s" && randomChoice === "r"){
+			return -1;
+	}else if (p1 ==="s" && randomChoice === "p"){
+			return 1;
+	}else if (p1 ==="s" && randomChoice === "s"){
 			return 0;
-		else //(randomChoice === "s"){
-			return -1;
 	}
-	else{ // if(playerChoice === "s")
-		if 	(randomChoice === "r")
-			return -1;
-		else if (randomChoice === "p")
-			return 1;
-		else// (randomChoice === "s"){
-			return 0;
-		}
-	}
+}
 
 while (loop === "y"){
 	for (var i = 1; i < 6; i++){
-			outcome = calculation();
-			playerChoice = Playerpick();
-			computerChoice = AIpick();
-			if (outcome === 1){
-				playerscore++;
-				AIscore--;
-				console.log(outcome);
-				console.log("Computer picked:"+computerChoice+" Player picked: "+playerChoice);
-				console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-				alert("Game: "+i+" Player Won!");
-				alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-			}
-			else if (outcome === -1){
-				playerscore--;
-				AIscore++;
-				console.log(outcome);
-				console.log("Computer picked:"+computerChoice+" Player picked: "+playerChoice);
-				console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-				alert("Game: "+i+" Player Won!");
-				alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-			}	
-			 else if (outcome === 0){
-				console.log(outcome);
-				console.log("Computer picked:"+computerChoice+" Player picked: "+playerChoice);
-				console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-				alert("Game: "+i+" Player Won!");
-				alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
-			}
+		var p1 = Playerpick();
+		var randomChoice = AIpick();
+		var outcome = calculation(p1,randomChoice);	
+		if (outcome === 1){
+			playerscore++;
+			AIscore--;
+			console.log(outcome);
+			console.log("Game: "+i+" Player Won!");
+			console.log("Computer picked:"+randomChoice+" Player picked: "+p1);
+			console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
+			alert("Game: "+i+" Player Won!");
+			alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
+		}	
+		else if (outcome === -1){
+			playerscore--;
+			AIscore++;
+			console.log(outcome);
+			console.log("Game: "+i+" Player Lost!");
+			console.log("Computer picked:"+randomChoice+" Player picked: "+p1);
+			console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
+			alert("Game: "+i+" Player Lost!");
+			alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);	
+		}
+		else{ // (outcome === 0){
+			console.log(outcome);
+			console.log("Game: "+i+" Player Tied!");
+			console.log("Computer picked:"+randomChoice+" Player picked: "+p1);
+			console.log("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
+			alert("Game: "+i+" Player Tied!");
+			alert("Current scores are: Player: "+playerscore+" and Computer: "+AIscore);
+		}
 	}
 		alert("Final scores best out of 5 games > Player: "+playerscore+" and Computer: "+AIscore);
-		console.log("Final scores best out of 5 games > Player: "+playerscore+" and Computer: "+AIscore);
+		console.log("Final score, best out of 5 games > Player: "+playerscore+" and Computer: "+AIscore);
 		playerscore = 0;
 		AIscore = 0;
-		loop = prompt("Run again, y/n?");
+		loop = prompt("Play again, y/n?");
 }
